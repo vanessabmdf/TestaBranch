393bc6df3fe20f3cefde1404f3762743ffa14509
cleitoncsg <cleiton.tom@hotmail.com>
2013-10-03 09:49:59 -0300

    Classe Pessoa
    classe modelo de Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,31 @@
+
+public class Pessoa {
+
+	public String nome;
+	public int idade;
+	public String sexo;
+	public Pessoa(String nome, int idade, String sexo) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.sexo = sexo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
